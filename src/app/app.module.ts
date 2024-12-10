271bafa3b6392c8162554fec2f468369fc4bb9b0
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2024-12-10 08:05:20 +0400

    templates, ContentChild, content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderModule } from './components/header/header.module';
 import { SidenavModule } from './components/sidenav/sidenav.module';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { SidenavModule } from './components/sidenav/sidenav.module';
         AppRoutingModule,
         HeaderModule,
         SidenavModule,
+        MatListModule,
     ],
     providers: [
         provideAnimationsAsync()
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import { MatList } from '@angular/material/list';
 import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
@@ -6,14 +7,40 @@ import { MatDrawer } from '@angular/material/sidenav';
     templateUrl: './sidenav.component.html',
     styleUrl: './sidenav.component.scss',
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit, AfterViewInit, OnChanges {
+
     @Input() isDrawerOpen = false;
     @Input() title?: string;
+    // @Input() template?: TemplateRef<unknown>;
     @Output() isDrawerOpenChange = new EventEmitter<boolean>();
 
-    @ViewChild('drawer') private readonly matdrawer?: MatDrawer;
+    @ViewChild('drawer', {static: true}) private readonly matdrawer?: MatDrawer;
+    // @ViewChild('viewPort', {static: true, read: ViewContainerRef}) private readonly viewPort?: ViewContainerRef;
+
+    // @ContentChild('matListClass', {static: true}) private readonly matList?: MatList; //не находит
+    @ContentChild('matListClass', {static: true, descendants: false})
+    private readonly matList?: MatList; // потомки найдутся
 
     toggleSidenavOpen () {
         this.matdrawer?.toggle();
+    }
+
+    ngOnInit(): void {
+        // console.log('ngOnInit', this.matdrawer)
+        // console.log('ngOnInit', this.viewPort)
+        console.log('ngOnInit', this.matList)
+        // if (this.matList){
+        //     this.viewPort?.createEmbeddedView(this.matList);
+        // }
+    }
+    ngAfterViewInit(): void {
+        // console.log('ngAfterViewInit', this.matdrawer)
+    }
+
+    ngOnChanges({template}: SimpleChanges): void {
+        // if(template && this.template) {
+        //     this.viewPort?.clear();
+        //     this.viewPort?.createEmbeddedView(this.template);
+        // }
     }
 }

f594a16581ae3e7b28bbffe761ea008537075cc3
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2024-12-25 08:08:55 +0400

    add services

---
+++ src/app/shared/products/products-store.service.ts
@@ -0,0 +1,25 @@
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {IProduct} from './product.interface';
+import {ProductsApiService} from './products-api.service';
+import { productsMock } from './products.mock';
+
+@Injectable()
+export class ProductsStoreService {
+    private readonly productsStore$ = new BehaviorSubject<IProduct[] | null>(null);
+
+    constructor(private readonly productsApiService: ProductsApiService) {}
+
+    get products$(): Observable<IProduct[] | null> {
+        return this.productsStore$.asObservable();
+    }
+
+    loadProducts() {
+        // this.productsApiService.getProducts$().subscribe(products => {
+        //     this.productsStore$.next(products);
+        // });
+        setTimeout(() => {
+            this.productsStore$.next(productsMock);
+        }, 1000);
+    }
+}

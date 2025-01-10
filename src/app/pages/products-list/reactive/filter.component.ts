a131df2cd7e747801a5e6aea639902b9df61ed70
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2025-01-10 08:37:30 +0400

    clean

--- src/app/pages/products-list/reactive/filter.component.ts
+++ src/app/pages/products-list/reactive/filter.component.ts
@@ -10,12 +10,6 @@ import {
 import {AbstractControl, FormArray, FormBuilder, FormControl, ValidationErrors, Validators} from '@angular/forms';
 import { map, Observable, startWith, timer } from 'rxjs';
 
-function isStringValidator(control: AbstractControl): ValidationErrors | null {
-    const {value} = control;
-
-    return Number(value) ? {isString: `Is not sting value - ${value}`} : null;
-}
-
 @Component({
     selector: 'app-filter',
     templateUrl: './filter.component.html',
@@ -27,34 +21,10 @@ export class FilterComponent implements OnChanges {
 
     @Output() changeFilter = new EventEmitter<any>();
 
-    protected controlStringReactive = new FormControl('', {
-        validators: [Validators.required, Validators.minLength(3),],
-        // validators: [Validators.required, Validators.minLength(3), isStringValidator],
-        asyncValidators: [this.isStringAsyncValidator.bind(this)],
-    });
-    protected stringTemplateForm = ''
-
-    errors$ = this.controlStringReactive.statusChanges.pipe(
-        map(() => this.controlStringReactive.errors),
-        startWith(this.controlStringReactive.errors),
-    );
-
     constructor(private readonly formBuilder: FormBuilder) {
-        this. controlStringReactive .valueChanges
-        .pipe(
-            startWith(this. controlStringReactive .value)
-        )
-        .subscribe((value) => {
-            console.log(value)
-        })
+
     }
 
     ngOnChanges({}: SimpleChanges) {
-    }
-
-    private isStringAsyncValidator(control: AbstractControl): Observable<ValidationErrors | null> {
-        console.log('isStringAsyncValidator');
-
-        return timer(2 * 1000).pipe(map(() => isStringValidator(control)));
     }
 }

e5ae35194e12e279a28fc8ed5f43dd1480bc5c74
Fedor Nikolaev <f.nikolaev@elma-bs.ru>
2025-02-21 08:33:01 +0400

    rename filter folder

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -7,7 +7,7 @@ import { IfModule } from '../../shared/if/if.module';
 import { RouterModule } from '@angular/router';
 import { CounterInputModule } from '../../shared/counter-input/counter-input.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FilterModule } from './reactive/filter.module';
+import { FilterModule } from './filter/filter.module';
 
 
 @NgModule({
